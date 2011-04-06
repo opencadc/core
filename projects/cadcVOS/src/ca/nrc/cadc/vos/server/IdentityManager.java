e7059383f266c48d9415539d1711db5f4260ecce
pdowler.cadc <pdowler.cadc@728ff76a-78ac-11de-a72b-d90af8dea425>
2011-04-06 23:02:58 +0000

    added IdentityManager interface and default X500 impl use IdentityManager to store (part of) calling Subject as node owner (put) and recreate partial Subject (get) store node owner in server-only NodeID and use that in VOSpaceAuthorizer use IdentityManager to convert Subject to string value for CREATOR property moved all Subject to owner code into DatabaseNodePersistence impl removed unused code from Node class
    git-svn-id: https://opencadc.googlecode.com/svn/trunk@1179 728ff76a-78ac-11de-a72b-d90af8dea425

---
+++ projects/cadcVOS/src/ca/nrc/cadc/vos/server/IdentityManager.java
@@ -0,0 +1,124 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2009.                            (c) 2009.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 4 $
+*
+************************************************************************
+*/
+
+package ca.nrc.cadc.vos.server;
+
+import javax.security.auth.Subject;
+
+/**
+ * Simple interface to manage the persistence of owner identity. Implementations
+ * determine which part of the caller subject the NodeDAO class will store (typically
+ * which Principal) and are responsible for for recreating the Subject again to support
+ * authorization checks and output of owner metadata.
+ *
+ * @author pdowler
+ */
+public interface IdentityManager 
+{
+    /**
+     * Create a subject from the specified owner object. This is the reverse
+     * of toOwner(Subject). The returned subject must include at least one
+     * Principal but need not contain any credentials.
+     *
+     * @param owner
+     * @return
+     */
+    Subject toSubject(Object owner);
+
+    /**
+     * Convert the specified subject into an arbitrary object. This is the reverse
+     * of toSubject(owner). The persistable object must capture the identity (the
+     * principal from the subject) but generally does not capture the credentials.
+     *
+     * @param subject
+     * @return arbitary owner object to be persisted
+     */
+    Object toOwner(Subject subject);
+
+    /**
+     * Get the SQL TYPE for the column that stores the owner object
+     * returned by toOwner(Subject);
+     *
+     * @see java.sql.Types
+     * @see java.sql.PreparedStatement.setObject(int,Object,int)
+     * @return a valid SQL type for use with a PreparedStatement
+     */
+    int getOwnerType();
+
+    /**
+     * Convert the specified subject to a suitable string representation of the
+     * owner. This should normally be an X509 distinguished name if IVOA
+     * single-sign-on has been implemented.
+     *
+     * @see VOS.PROPERTY_URI_CREATOR
+     * @param subject
+     * @return string representation of the owner (principal)
+     */
+    String toOwnerString(Subject subject);
+}
