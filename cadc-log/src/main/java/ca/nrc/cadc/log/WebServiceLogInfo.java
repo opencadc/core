ea334bf21f41390d16673529a41e242c217e2ea3
Helena Jeeves <hridolfo@gmail.com>
2018-05-11 11:57:50 -0700

    rt 75374: added setPath function to WebServiceLogInfo as it's not always part of information used in constructors

--- cadc-log/src/main/java/ca/nrc/cadc/log/WebServiceLogInfo.java
+++ cadc-log/src/main/java/ca/nrc/cadc/log/WebServiceLogInfo.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2016.                            (c) 2016.
+*  (c) 2018.                            (c) 2018.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -268,6 +268,10 @@ public abstract class WebServiceLogInfo
         }
 
         return ANONYMOUS_USER;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 
 }
