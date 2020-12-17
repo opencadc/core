b28ececf471e853ea6ee2e317df96345595ddd07
Helena Jeeves <Helena.Jeeves@nrc-cnrc.gc.ca>
2020-12-17 07:30:27 -0800

    CADC-8954: fixed check for -1 error never reached.

--- cadc-util/src/main/java/ca/nrc/cadc/net/HttpTransfer.java
+++ cadc-util/src/main/java/ca/nrc/cadc/net/HttpTransfer.java
@@ -760,7 +760,7 @@ public abstract class HttpTransfer implements Runnable {
         
         captureResponseHeaders(conn);
         
-        if (responseCode < 400) {
+        if (responseCode <= 100 && responseCode < 400) {
             return;
         }
         log.debug("error: " + contentType + " " + contentLength);
