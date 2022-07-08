b142f83d48f7be35f422fb2127b27f7fb28f277a
Patrick Dowler <pdowler.cadc@gmail.com>
2022-07-08 08:47:09 -0700

    cadc-http-client: added --token and --domain support

--- cadc-http-client/src/main/java/ca/nrc/cadc/net/Main.java
+++ cadc-http-client/src/main/java/ca/nrc/cadc/net/Main.java
@@ -65,12 +65,12 @@
 *  $Revision: 5 $
 *
 ************************************************************************
-*/
+ */
 
 package ca.nrc.cadc.net;
 
-
 import ca.nrc.cadc.auth.AuthenticationUtil;
+import ca.nrc.cadc.auth.AuthorizationToken;
 import ca.nrc.cadc.auth.CertCmdArgUtil;
 import ca.nrc.cadc.auth.RunnableAction;
 import ca.nrc.cadc.net.event.TransferEvent;
@@ -82,6 +82,7 @@ import java.io.FileReader;
 import java.io.LineNumberReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import javax.security.auth.Subject;
 import org.apache.log4j.Level;
@@ -91,100 +92,110 @@ import org.apache.log4j.Logger;
  *
  * @author pdowler
  */
-public class Main implements TransferListener
-{
+public class Main implements Runnable, TransferListener {
+
     private static final Logger log = Logger.getLogger(Main.class);
 
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             ArgumentMap am = new ArgumentMap(args);
-            if (am.isSet("h") || am.isSet("help"))
+            if (am.isSet("h") || am.isSet("help")) {
                 usage();
+            }
             Level level = Level.WARN;
-            
-            if (am.isSet("d") || am.isSet("debug"))
+
+            if (am.isSet("d") || am.isSet("debug")) {
                 level = Level.DEBUG;
-            else if (am.isSet("v") || am.isSet("verbose"))
+            } else if (am.isSet("v") || am.isSet("verbose")) {
                 level = Level.INFO;
-            
+            }
+
             Log4jInit.setLevel("ca.nrc.cadc.net", level);
             Log4jInit.setLevel("ca.nrc.cadc.auth", level);
-            
+
             Subject s = AuthenticationUtil.getAnonSubject();
             if (am.isSet("cert")) {
                 s = CertCmdArgUtil.initSubject(am);
+            } else if (am.isSet("token")) {
+                s = getSubjectWithToken(am.getValue("token"), am.getValue("domain"));
             }
-            
+
             List<String> urls = am.getPositionalArgs();
             String fname = am.getValue("in");
-            if (fname != null)
-            {
+            if (fname != null) {
                 File f = new File(fname);
                 LineNumberReader r = new LineNumberReader(new FileReader(fname));
                 String line = r.readLine();
-                while (line != null)
-                {
+                while (line != null) {
                     String[] tokens = line.split(" ");
-                    if (tokens.length == 1)
+                    if (tokens.length == 1) {
                         urls.add(tokens[0]);
-                    else
+                    } else {
                         log.info("ignoring line: " + line);
+                    }
                     line = r.readLine();
                 }
             }
-            Main m = new Main(s, urls);
-            m.run();
-        }
-        catch(Throwable t)
-        {
+            Main m = new Main(urls);
+            Subject.doAs(s, new RunnableAction(m));
+        } catch (Throwable t) {
             log.error("FAIL", t);
         }
     }
-    
-    private static void usage()
-    {
+
+    private static Subject getSubjectWithToken(String token, String domain) {
+        String[] ss = token.split(":");
+        String tokenType = ss[0];
+        String tokenValue = ss[1];
+        List<String> domains = new ArrayList<>();
+        domains.add(domain);
+        AuthorizationToken atk = new AuthorizationToken(tokenType, tokenValue, domains);
+        Subject s = new Subject();
+        s.getPublicCredentials().add(atk);
+        return s;
+    }
+
+    private static void usage() {
         System.out.println("usage: cadc-http-client [-v|--verbose|-d|--debug] [--in=<fname>] [<url> ...]");
         System.out.println("         --in : file name with URLs (one per line)");
         System.out.println("         <url> : URLs directly on the command line");
+        System.out.println();
+        System.out.println(" auth options");
+        System.out.println("         --cert=<file> : file name with URLs (one per line)");
+        System.out.println();
+        System.out.println("         --token  : a token in the form {name}:{value} (e.g. bearer:blah-blah)");
+        System.out.println("         --domain : domain the token should me sent to");
         System.exit(1);
     }
-    
+
     private Subject subject;
     private List<String> urls;
-    
-    private Main() { }
-    private Main(Subject s, List<String> urls)
-    {
-        this.subject = s;
+
+    private Main() {
+    }
+
+    private Main(List<String> urls) {
         this.urls = urls;
     }
-    public void run()
-    {
-        for (String surl : urls)
-        {
-            try
-            {
+
+    public void run() {
+        for (String surl : urls) {
+            try {
                 URL url = new URL(surl);
                 File dest = new File(System.getProperty("user.dir"));
                 HttpDownload doit = new HttpDownload(url, dest);
                 doit.setOverwrite(true);
                 doit.setTransferListener(this);
-                Subject.doAs(subject, new RunnableAction(doit));
-            }
-            catch(MalformedURLException ex)
-            {
+                doit.run();
+            } catch (MalformedURLException ex) {
                 log.error("invalid input URL: " + surl);
             }
         }
     }
 
     @Override
-    public void transferEvent(TransferEvent te)
-    {
-        switch(te.getState())
-        {
+    public void transferEvent(TransferEvent te) {
+        switch (te.getState()) {
             case TransferEvent.CONNECTED:
             case TransferEvent.DECOMPRESSING:
             case TransferEvent.TRANSFERING:
@@ -206,10 +217,8 @@ public class Main implements TransferListener
     }
 
     @Override
-    public String getEventHeader()
-    {
+    public String getEventHeader() {
         return "";
     }
-    
-    
+
 }
